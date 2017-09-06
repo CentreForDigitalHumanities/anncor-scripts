bf6ce32526e95228a27e8746b4e45147a8a6e02a
Sheean Spoel <s.j.j.spoel@uu.nl>
2017-09-06 15:05:19 +0200

    Implemented windowed selection algorithm. Resolves #1

--- selection/window.py
+++ selection/window.py
@@ -1,23 +1,42 @@
+"""
+Windowed file selection: select a range from a file with the highest associated total score.
+"""
 import math
 
 
-def get_selection(scores, fraction):
+def get_selection(utterances, fraction):
     """
     Return the windowed selection with the highest score.
 
     Arguments:
-    scores -- a list of scores and their index number (number,score)[]
+    utterances -- a list of utterance numbers and their scores (number,score)[]
     fraction -- the fraction size to select
     """
-    subset_length = math.ceil(fraction * len(scores))
-    return [] # TODO
+    subset_length = math.ceil(fraction * len(utterances))
+
+    window = utterances[:subset_length]
+    # the initial window might be the highest scoring
+    selection = list(window)
+    window_score = sum(score for number, score in window)
+    highest_score = window_score
+
+    for (utterance_number, utterance_score) in utterances[subset_length:]:
+        (_, first_score) = window.pop(0)
+        window_score -= first_score - utterance_score
+        window.append((utterance_number, utterance_score))
+        if window_score > highest_score:
+            selection = list(window)
+            highest_score = window_score
+
+    return list(number for number, score in selection)
+
 
 def get_file_selection(files, fraction):
     """
     Return the files with the window selections having the highest score.
 
     Arguments:
-    scores -- a list of filenames, scores and their index number (filename, (number,score)[])[]
+    files -- a list of filenames, utterances number and their scores (filename, (number,score)[])[]
     fraction -- the fraction size to select
     """
-    return [] # TODO
+    return [(filename, get_selection(utterances, fraction)) for filename, utterances in files]
